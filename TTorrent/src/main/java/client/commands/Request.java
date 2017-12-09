6e72993a3e2c76459f1f83b8db192416be02370c
ekaterinaMaljutina <e.maljutina@gmail.com>
2017-12-09 22:03:42 +0300

    impl upload command

---
+++ TTorrent/src/main/java/client/commands/Request.java
@@ -0,0 +1,9 @@
+package client.commands;
+
+
+public class Request {
+    public static final Integer LIST_REQUEST = 1;
+    public static final Integer UPLOAD_REQUEST = 2;
+    public static final Integer SOURCES_REQUEST = 3;
+    public static final Integer UPDATE_REQUEST = 4;
+}
--- TTorrent/src/main/java/client/commands/UploadCommand.java
+++ TTorrent/src/main/java/client/commands/UploadCommand.java
@@ -1,9 +1,15 @@
 package client.commands;
 
 import client.state.api.IState;
+import common.files.FileInfoImpl;
+import common.files.IFileInfo;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class UploadCommand implements ICommand {
 
@@ -15,12 +21,46 @@ public class UploadCommand implements ICommand {
 
     @Override
     public void runCommand(@NotNull IState state, String[] args) {
-        
-        //        server.upload();
+        // args : id_command file_name_for_download
+
+        if (args.length < 2) {
+            LOGGER.error(" upload without args : <2> file_name");
+            return;
+        }
+
+        IFileInfo fileInfo = getFileInfo(Paths.get(args[1]), state);
+        if (fileInfo == null) {
+            return;
+        }
+
+        try {
+            int idxFile = server.upload(fileInfo);
+            LOGGER.debug(String.format(
+                    " upload file : %s -> id_file : %d",
+                    fileInfo.getName(), idxFile));
+
+            // state add new file !!!!!
+
+        } catch (IOException e) {
+            LOGGER.error(" not upload file " + args[1]);
+            LOGGER.error(e);
+        }
+
     }
 
     @Override
     public String commandName() {
         return "Upload Request To Server";
+    }
+
+
+    private static @Nullable
+    IFileInfo getFileInfo(@NotNull Path pathToFile, @NotNull IState currentState) {
+        if (currentState.getAllFilesWithInfo().containsKey(pathToFile) || !pathToFile.toFile().exists()) {
+            LOGGER.error(String.format("file_name = %s : file already exist or file not found", pathToFile.toString()));
+            return null;
+        }
+
+        return new FileInfoImpl(pathToFile.toFile().getName(), pathToFile.toFile().length());
     }
 }
